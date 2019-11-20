64eed0635db9a6274f7475eb6690074e86799cdf
viccosta <victor_costa@rogers.com>
2019-11-20 13:41:10 -0500

    Update: 1.1 With Winner Display

--- src/index.js
+++ src/index.js
@@ -35,7 +35,11 @@ class Board extends React.Component
     
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext ? 'X' : '0';
+        if(calculateWinner(squares) || squares[i])
+        {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
             xIsNext: !this.state.xIsNext,
@@ -54,8 +58,20 @@ class Board extends React.Component
 
     render()
     {
-        const status = 'Next player: X';
-
+        const winner = calculateWinner(this.state.squares);
+        //-----------------------------------------
+        let appVersion = 'Version 1.1';
+        //-----------------------------------------
+        let status;
+        if(winner)
+        {
+            status = "Winner: " + winner;
+        }
+        else
+        {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+        //-----------------------------------------
         return (
             <div>
                 <div className="status">{status}</div>
@@ -74,6 +90,7 @@ class Board extends React.Component
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
                 </div>
+                <div className="appVersion">{appVersion}</div>
             </div>
         );
     }
@@ -108,3 +125,33 @@ ReactDOM.render
     <Game />,
     document.getElementById('root')
 );
+//-----------------------------------------------
+//----
+//----
+//-----------------------------------------------
+function calculateWinner(squares) 
+{
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for(let i = 0; i < lines.length; i++)
+    {
+        const [a,b,c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+        {
+            return squares[a];
+        }
+    }
+    return null;
+}
+//-----------------------------------------------
+//----
+//----
+//-----------------------------------------------
