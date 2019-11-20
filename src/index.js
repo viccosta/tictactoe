184794e24f53f65431b65c11e25d9da0d0c39c89
viccosta <victor_costa@rogers.com>
2019-11-20 14:23:30 -0500

    Update Version 1.2

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ class Board extends React.Component
     {
         const winner = calculateWinner(this.state.squares);
         //-----------------------------------------
-        let appVersion = 'Version 1.1';
+        let appVersion = 'Version 1.2';
         //-----------------------------------------
         let status;
         if(winner)
